9b762d9e40ca4c6b4a8f0a35bb9e0c989d395a3a
RedJone888 <redjoan.guo@gmail.com>
2025-04-15 23:20:47 +0800

    修改publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/weather-app/' : '/'
+  publicPath: (() => {
+    if (process.env.NODE_ENV === 'production') {
+      if (process.env.DEPLOY_TARGET === 'github-pages') {
+        return '/weather-app/'
+      } else if (process.env.DEPLOY_TARGET === 'vercel') {
+        return '/'
+      }
+    }
+    return '/' // 默认开发环境
+  })()
 })
